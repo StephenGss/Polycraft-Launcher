823a554cfa55514a606a84aed5c003940a9fea3b
Stephen Goss <Stephengs94@gmail.com>
2019-03-05 15:04:19 -0600

    re-enable analytics using polycraft analytics id

--- application/MultiMC.cpp
+++ application/MultiMC.cpp
@@ -1134,7 +1134,7 @@ MainWindow* MultiMC::showMainWindow(bool minimized)
         m_openWindows++;
     }
     // FIXME: move this somewhere else...
-    if(false)
+    if(m_analytics)
     {
         auto windowSize = m_mainWindow->size();
         auto sizeString = QString("%1x%2").arg(windowSize.width()).arg(windowSize.height());
